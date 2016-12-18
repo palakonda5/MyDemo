3f083e290b1c583c7fc0f453d766cf5c90de0cef
root <root@ip-172-31-59-147.ec2.internal>
2016-12-18 11:38:58 +0000

    modified script

--- test.sh
+++ test.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo "hiii paparao"
+
+clear
+
+echo "The script starts now"
+USER="Rao"
+echo "Hi, $USER!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo 
+w
+echo
+
+echo "Im setting two variables now"
+COLOUR="black"
+VALUE="9"
+echo "This is a string: $COLOUR"
+echo "And this is a number : $VALUE"
+echo
+
+echo "Im giving you back your prompt now."
+echo
